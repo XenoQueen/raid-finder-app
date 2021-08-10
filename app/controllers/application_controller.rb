909dfefdbb7728304f0e199b22e8f679aeea88f3
XenoQueen <queenmab051@gmail.com>
2021-08-10 03:46:28 -0700

    Defines current_player method in application controller and applies to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-
+    before_action :current_player
     def home
+    end
+
+    def current_player
+        if session[:current_player_id]
+            @current_player = Player.find(session[current_player_id])
+        end
     end
 end
