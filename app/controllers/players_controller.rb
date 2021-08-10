a19619945be79a0663ca8a28829fa6a91cabf078
XenoQueen <queenmab051@gmail.com>
2021-08-10 03:26:15 -0700

    Defines new player method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,9 @@ class PlayersController < ApplicationController
 
     #GET /register - New Player Action
     def new
-        
+        if session[:current_player_id]
+            redirect_to "/", :notice => "This player is already logged in."
+        end
     end
 
     # POST /players - Create the Player Action
