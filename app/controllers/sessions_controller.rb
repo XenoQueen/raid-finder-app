866208f3f5f074cacf2fc052a734e5eb6c676b98
XenoQueen <queenmab051@gmail.com>
2021-08-10 03:08:56 -0700

    Defines create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @player = Player.find_by(:playername => params[:playername])
+        if @player
+            raise "login".inspect
+        else
+            redirect_to "login", :message => "Could not find player, please try again."
+        end
+    end
 end
