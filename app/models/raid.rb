82cc31ff5d854a93c590846d070a05b50b66b096
XenoQueen <queenmab051@gmail.com>
2021-08-12 04:50:43 -0700

    Defines by_status method

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -2,6 +2,15 @@ class Raid < ApplicationRecord
 belongs_to :leader_player, :class_name => "Player"
 belongs_to :member_player, :class_name => "Player", :optional => true
 
+    def self.by_status(status)
+        case status
+        when :open
+            where(:member_player => nil)
+        when :closed
+            where.not(:member_player => nil)
+        end
+    end
+
     def accepted_by(player)
         self.update(:member_player => current_player)
     end
