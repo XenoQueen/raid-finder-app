7e59f8f82b75d574e767a8ff74c34bdd6c1d7612
XenoQueen <queenmab051@gmail.com>
2021-08-12 02:45:29 -0700

    Adds associations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+    has_many :raid_requests, :class_name "Raid", :foreign_key => "leader_player_id"
+    has_many :raid_responses, :class_name "Raid", :foreign_key => "member_player_id"
 end
