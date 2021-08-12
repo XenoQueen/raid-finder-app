3f08fc994d1c0df534d1384d542c34bf91654dd8
XenoQueen <queenmab051@gmail.com>
2021-08-12 04:35:29 -0700

    Defines accepted_by method

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -2,5 +2,7 @@ class Raid < ApplicationRecord
 belongs_to :leader_player, :class_name => "Player"
 belongs_to :member_player, :class_name => "Player", :optional => true
 
-
+    def accepted_by(player)
+        self.update(:member_player => current_player)
+    end
 end
