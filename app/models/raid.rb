de45c1783ff89af623dbf1de5fc2276dac5e2a4b
XenoQueen <queenmab051@gmail.com>
2021-08-12 05:15:13 -0700

    Fixed syntax error

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -1,6 +1,7 @@
 class Raid < ApplicationRecord
 belongs_to :leader_player, :class_name => "Player"
 belongs_to :member_player, :class_name => "Player", :optional => true
+scope :by_status, -> (status) {where("created_at < ?", time)}
 
     def self.by_status(status)
         case status
