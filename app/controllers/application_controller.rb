49531e4f072cf95c7add61efa993cb8b58d36e8a
XenoQueen <queenmab051@gmail.com>
2021-08-12 02:42:51 -0700

    Defines authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ include ApplicationHelper::SessionsHelper
     
     def home
     end
+
+    private
+
+    def authentication_required
+        if !logged_in?
+            redirect_to "/login"
+        end
+    end
 end
