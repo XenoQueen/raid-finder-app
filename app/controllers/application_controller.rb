0e7f8b0c0c64221f24c3c3bfcff31e606604917a
XenoQueen <queenmab051@gmail.com>
2021-08-12 01:02:40 -0700

    Moves helper methods to helper file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
+include ApplicationHelper::SessionsHelper
+
     before_action :current_player
+    
     def home
-    end
-
-    def current_player
-        if session[:current_player_id]
-            @current_player = Player.find(session[current_player_id])
-        end
-    end
-
-    def login(player)
-        session[:current_player_id] = @player.id
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    module SessionsHelper
+
+        def current_player
+            if session[:current_player_id]
+                @current_player = Player.find(session[current_player_id])
+            end
+        end
+
+        def login(player)
+            session[:current_player_id] = @player.id
+        end
+    end
 end
